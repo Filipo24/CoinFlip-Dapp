a13b13d6cbdfc58bac4598f9c411c312ded33161
Filipo24 <68088450+Filipo24@users.noreply.github.com>
2020-07-18 16:21:05 +0100

    Add files via upload

--- migrations/2_coinflip.js
+++ migrations/2_coinflip.js
@@ -1,5 +1,5 @@
 const Coinflip = artifacts.require("Coinflip");
 
 module.exports = function(deployer) {
-  deployer.deploy(Coinflip, {value: web3.utils.toWei("10", "ether")});
+  deployer.deploy(Coinflip, {value: web3.utils.toWei("0.5", "ether")});
 };
