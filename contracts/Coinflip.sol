0710f7fb2459b3c8953e93a4e388c1bb491bd726
Filipo24 <68088450+Filipo24@users.noreply.github.com>
2020-07-18 16:20:50 +0100

    Add files via upload

--- contracts/Coinflip.sol
+++ contracts/Coinflip.sol
@@ -1,97 +1,89 @@
 pragma solidity 0.5.12;
+import "./provableAPI.sol";
+import "./Ownable.sol";
 
-contract Coinflip{
+contract Coinflip is Ownable, usingProvable {
 
   address owner;
-  uint public contractBalance;
   uint private senderBetValue;
+  uint256 public latestNumber;
+  uint256 constant NUM_RANDOM_BYTES_REQUESTED = 1;
 
-
-  constructor() public payable {
-    owner = msg.sender;
-    contractBalance = msg.value;
-  }
 
   modifier costs(uint cost){
       require(msg.value >= cost);
       _;
   }
 
-  struct Gambler {
+
+  struct Player {
+    address payable playerAddress;
     uint betValue;
     uint coinGuess;
-    uint result;
+    bool statusOfCallback;
+    uint256 result;
   }
 
-  mapping (address => Gambler) private bets;
 
-  event Result (address player, bool betResult, uint valueBack);
+  mapping(bytes32=> Player) playerIds;
 
+  event Result (address indexed player, bool betResult, uint valueBack);
+  event Withdraw (address owner, uint valueWithdrawn);
+  event LogNewProvableQuery(string description);
+  event GeneratedRandomNumber(uint256 randomNumber);
 
-    function random() public view returns (uint){
-      //function to generate 1 or 0 to indicate win
-        return now % 2;
-    }
+  function getBalance() public view returns(uint){
+    return address(this).balance;
+  }
 
+  function fundContract() public payable onlyOwner {
+  }
 
+  function withdrawAll() public onlyOwner  {
+    uint toTransfer = address(this).balance;
+    msg.sender.transfer(toTransfer);
+    emit Withdraw(msg.sender, toTransfer);
+   }
 
-    function placeBet(uint coinSelection) public payable costs(1 wei) returns(bool betResult){
-      //receive ETH into the contract as a bet and make a decision if someone won
-      /*balance += msg.value;*/
-      Gambler memory newGambler;
+  function startGame(uint coinSelection) public payable costs(1 wei) {
+    //receive ETH into the contract as a bet and make a call to usingProvable oracle for random number and store queryID into a mapping + all the valuesinto Player structure
+    Player memory newPlayer;
+    bytes32 playerQueryId;
+    newPlayer.playerAddress = msg.sender;
+    newPlayer.betValue = msg.value;
+    newPlayer.coinGuess = coinSelection;
+
+    playerQueryId = getRandomNumber();
+    newPlayer.statusOfCallback = true;
+    playerIds[playerQueryId] = newPlayer;
+  }
+
+  function getRandomNumber() public payable returns (bytes32) {
+    uint256 QUERY_EXECUTION_DELAY = 0;
+    uint256 GAS_FOR_CALLBACK = 200000;
+
+    bytes32 realQueryId =  provable_newRandomDSQuery(QUERY_EXECUTION_DELAY, NUM_RANDOM_BYTES_REQUESTED, GAS_FOR_CALLBACK );
+
+    emit LogNewProvableQuery("Provable query was sent, standing by for the answer..");
+    return realQueryId;
+  }
+
+  function __callback(bytes32 _queryId, string memory _result, bytes memory _proof) public {
+      require(playerIds[_queryId].statusOfCallback == true,"Invalid callback status");
+      require(msg.sender == provable_cbAddress(), "Invalid address for callback");
       uint toTransfer;
-      address payable gambler = msg.sender;
-      newGambler.betValue = msg.value;
-      newGambler.coinGuess = coinSelection;
-      newGambler.result = random();
-      bets[gambler] = newGambler;
 
-      if (bets[gambler].result == bets[gambler].coinGuess){
-        toTransfer = bets[gambler].betValue * 2;
-        gambler.transfer(toTransfer);
-        emit Result(msg.sender, true, toTransfer);
-        return true;
+      uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % 2;
+      playerIds[_queryId].result = randomNumber;
+      emit GeneratedRandomNumber(randomNumber);
+
+      if (playerIds[_queryId].result == playerIds[_queryId].coinGuess){
+        toTransfer = playerIds[_queryId].betValue * 2;
+        playerIds[_queryId].playerAddress.transfer(toTransfer);
+        emit Result(playerIds[_queryId].playerAddress, true, toTransfer);
       }else{
-        /*emit Result("You lost, try next time!", msg.sender);*/
-        emit Result(msg.sender, false, 0);
-        return false;
+        emit Result(playerIds[_queryId].playerAddress, false, 0);
       }
 
-    }
-
-
-    /*function result() public returns (bool betResult){
-      //function to generate 1 or 0 to indicate win
-      address gambler = msg.sender;
-      uint toTransfer = bets[gambler].betValue;
-      if (bets[gambler].result == bets[gambler].coinGuess){
-        msg.sender.transfer(toTransfer*2);
-        emit Result(msg.sender, true);
-        return true;
-      }else{
-        emit Result("You lost, try next time!", msg.sender);
-        emit Result(msg.sender, false);
-        return false;
-      }
-    }*/
-
-    /*function insertGambler(Gambler memory newGambler, address ) private {
-    address gambler = msg.sender;
-    bets[gambler] = newGambler;
-    }
-
-    function sendWinnings(uint betValue) public returns (uint) {
-      //send 2xETH back if user won
-      address winner = msg.sender;
-      uint toTransfer = bets[winner];
-      balance = balance - toTransfer;
-      msg.sender.transfer(toTransfer);
-      deleteBet(sender);
-      return toTransfer;
-    }
-
-    function deleteBet(address creator) private {
-        delete bets[creator];
-        assert(bets[creator] == 0);
-    }*/
+  }
 }
